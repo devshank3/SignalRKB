1c6a76b7715975676ba374d8f1196486512af1bd
Shankar Hariharan K <devshank3@gmail.com>
2024-12-14 12:08:52 +0530

    A project for choosing different transport types and a blog on that

---
+++ SignalRmestaruus/Essentials_Getting_Started/ChooseTransportTypes/BasicClientServer/Hubs/AccessorHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BasicClientServer.Hubs
+{
+    public class AccessorHub : Hub
+    {
+        public static int ViewCount { get; set; } = 0;
+
+        public async Task NotifyWatching()
+        {
+            ViewCount++;
+
+            // Notify all clients
+            await Clients.All.SendAsync("UpdateViewCount", ViewCount);
+        }
+
+    }
+}
---
+++ docs/SignalR/ChoosingTransportOption.md
@@ -0,0 +1,12 @@
+
+
+Tips to fake making websocket not available in browser, 
+
+```JavaScript
+import * as signalR from "@microsoft/signalr";
+
+WebSocket = undefined;
+
+...
+...
+```
