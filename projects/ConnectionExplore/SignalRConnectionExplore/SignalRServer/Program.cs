b03c0bbb9326214b6d5be02dda5e633673517721
Shankar Hariharan K <devshank3@gmail.com>
2024-12-17 18:45:26 +0530

    Add initial SignalR server setup with basic configuration
    Added a new Visual Studio solution `SignalRConnectionExplore.sln` with a project named "SignalRServer". Implemented a `ChatHub` class in `ChatHub.cs` for handling real-time messaging. Created a `Program.cs` file to set up an ASP.NET Core application with SignalR support. Included `launchSettings.json` for environment configurations. Added `SignalRServer.csproj` specifying the project as a .NET 8.0 web application. Added `SignalRServer.http` for HTTP request definitions. Included `appsettings.Development.json` and `appsettings.json` for logging and host configurations.

---
+++ projects/ConnectionExplore/SignalRConnectionExplore/SignalRServer/Program.cs
@@ -0,0 +1,31 @@
+using SignalRServer.Hubs;
+
+namespace SignalRServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddSignalR();
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapHub<ChatHub>("/Chat");
+
+
+            app.Run();
+        }
+    }
+}
